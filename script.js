cb5ccd7e43057be30f38e7bd244ee60405a76e46
imCyberMohsin <mohsinansari7033@gmail.com>
2023-08-14 19:48:32 +0530

    colors added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //* Script For Increment-Decrement Counter  
 
+const container = document.querySelector('#container');
+const headH2 = document.querySelector('#heading');
 const count_heading = document.querySelector('#count-heading');
 const inc_btn = document.querySelector('#increment');
 const dec_btn = document.querySelector('#decrement');
@@ -10,10 +12,14 @@ inc_btn.addEventListener('click', () => {
     count++;
     count_heading.innerHTML = count;
     if (count === 0) {
+        headH2.style.color = 'white';
         count_heading.style.color = 'white';
+        container.style.boxShadow = `2px 2px 2px 2px white`;
     }
     else if (count > 0) {
+        headH2.style.color = 'Green';
         count_heading.style.color = 'Green';
+        container.style.boxShadow = `2px 2px 2px 2px green`;
     }
 })
 
@@ -21,9 +27,13 @@ dec_btn.addEventListener('click', () => {
     count--;
     count_heading.innerHTML = count;
     if (count === 0) {
+        headH2.style.color = 'white';
         count_heading.style.color = 'white';
+        container.style.boxShadow = `2px 2px 2px 2px white`;
     }
     else if (count < 0) {
+        headH2.style.color = 'crimson';
         count_heading.style.color = 'crimson';
+        container.style.boxShadow = `2px 2px 2px 2px crimson`;
     }
 })
