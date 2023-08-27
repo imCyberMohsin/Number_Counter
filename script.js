ca67c1c3ed9fccaa0dec45bbee0305029634f401
imCyberMohsin <mohsinansari7033@gmail.com>
2023-08-27 12:04:58 +0530

    reset btn added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//* Script For Increment-Decrement Counter  
+//* Counter Script  
 
 const container = document.querySelector('#container');
 const headH2 = document.querySelector('#heading');
 const count_heading = document.querySelector('#count-heading');
 const inc_btn = document.querySelector('#increment');
 const dec_btn = document.querySelector('#decrement');
+const reset_btn = document.querySelector('#reset');
 
 let count = 0;
 
+// Increment Button
 inc_btn.addEventListener('click', () => {
     count++;
     count_heading.innerHTML = count;
@@ -23,6 +25,7 @@ inc_btn.addEventListener('click', () => {
     }
 })
 
+// Decrement Button
 dec_btn.addEventListener('click', () => {
     count--;
     count_heading.innerHTML = count;
@@ -35,5 +38,16 @@ dec_btn.addEventListener('click', () => {
         headH2.style.color = 'crimson';
         count_heading.style.color = 'crimson';
         container.style.boxShadow = `2px 2px 2px 2px crimson`;
+    }
+})
+
+// Reset Button
+reset_btn.addEventListener('click', () => {
+    count = 0
+    count_heading.innerHTML = count;
+    if (count === 0) {
+        headH2.style.color = 'white';
+        count_heading.style.color = 'white';
+        container.style.boxShadow = `2px 2px 2px 2px white`;
     }
 })
